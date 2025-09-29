aa9c57bf8535baef5b936c05f7dfda458f8c5379
Matvii Blyzniuk <matvii.blyzniuk@pro-bit.si>
2025-09-29 17:22:58 +0300

    test 1

---
+++ test_1.py
@@ -0,0 +1,30 @@
+# make 41
+question_1 = [82, 14, 7, 23, 5]
+
+
+# change "English" to "английски" in the text below
+question_2 = "Когда меня спрашивают про языки, я всегда отвечаю: я говорю по English. Друзья смеются, что я слишком горжусь, но я снова повторяю: я говорю по English. И если нужно, скажу ещё раз — я говорю по English"
+
+
+# посчитай площадь каждого из прямоугольников и запиши в переменные(wigth * length),
+# просумируй их
+question_3_1 = {"width": 5, "length": 10}
+question_3_2 = {"width": 8, "length": 12}
+question_3_3 = {"width": 3, "length": 7}
+
+
+# узнай длину question_2
+
+
+# что выведет этот код?
+# question_5 = {1, 2, 3, 3}
+# print(s)
+
+
+
+# что выведет этот код?
+s = True
+a = 1
+print(s)
+print(a)
+print(s+a)
