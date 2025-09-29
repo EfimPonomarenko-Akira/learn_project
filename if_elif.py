16e4e60a3da055af647043e24a8cf4c74e1745ae
Matvii Blyzniuk <matvii.blyzniuk@pro-bit.si>
2025-09-29 18:46:24 +0300

    if elif else

---
+++ if_elif.py
@@ -0,0 +1,23 @@
+словарь = 1000
+a = 300
+x = 100
+
+
+string = "stoka"
+integer = 1
+float = 1.1
+list = [1,2,3]
+dictionary = {"key": "value"}
+set = {1,2,3}
+tuple = (1,2,3)
+bool = True
+
+peremennaya = 2
+
+
+if словарь > a:
+    print("словарь больше A")
+elif словарь > x:
+    print("словарь больше X")
+else:
+    print("словарь меньше")
